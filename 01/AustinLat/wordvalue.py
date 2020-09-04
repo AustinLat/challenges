231deb9ced18ea1ed6bbc5259dee4c043760d0ce
Austin Laturno <alaturno@gmail.com>
2020-09-03 18:59:23 -0700

    hangman

--- 01/AustinLat/wordvalue.py
+++ 01/AustinLat/wordvalue.py
@@ -2,33 +2,17 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    with open(DICTIONARY) as file:
-        contents = file.read()
-        words = contents.splitlines()
-    return words
+    pass
 
-def calc_word_value(word):
+def calc_word_value():
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
+    pass
 
-    letter_to_score = [LETTER_SCORES[letter.upper()] for letter in list(word)
-                                        if letter.upper() in LETTER_SCORES.keys()]
-    return sum(letter_to_score)
-
-
-def max_word_value(list_of_words=None):
+def max_word_value():
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    if not list_of_words:
-        list_of_words = load_words()
-
-    word_to_score_list = [{'word': word, 'score': calc_word_value(word)}
-                                                for word in list_of_words]
-
-    sorted_words_to_score = sorted(word_to_score_list, key= lambda word_score: word_score['score'])
-
-    return sorted_words_to_score[-1]['word']
-
+    pass
 
 if __name__ == "__main__":
     pass # run unittests to validate
