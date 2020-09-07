1fd4be6a597d98e7af39da218cdf19e5c8024643
Austin Laturno <alaturno@gmail.com>
2020-09-06 22:04:17 -0700

    First test passed

--- 01/AustinLat/wordvalue.py
+++ 01/AustinLat/wordvalue.py
@@ -2,16 +2,16 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    dic = open(DICTIONARY, 'r')
-    diclist = [line.split() for line in dic.readlines()]
-    return diclist
+    dictionary = []
+    with open(DICTIONARY, 'r') as f:
+        list = f.readlines()                                                            # The following creates a list by reading each line of my .txt file.
+        list = [x.strip('\n') for x in list]
+    return list
 
-
-
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    # for i in word:
 
 def max_word_value():
     """Calculate the word with the max value, can receive a list
