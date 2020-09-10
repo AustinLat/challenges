87541e2e23b660a075bd3ba38d244b4ba63a120d
Austin Laturno <alaturno@gmail.com>
2020-09-09 19:00:59 -0700

    all test passed!

--- 01/AustinLat/wordvalue.py
+++ 01/AustinLat/wordvalue.py
@@ -1,5 +1,6 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words():
     """Load dictionary into a list and return list"""
     dictionary = []
@@ -8,23 +9,28 @@ def load_words():
         list = [i.strip('\n') for i in list]
     return list
 
+
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
     score = 0
     for i in word.upper():
-        letter = i
-        print(letter)
-        if letter in LETTER_SCORES:
-            score += LETTER_SCORES[letter]
+        if i in LETTER_SCORES:
+            score += LETTER_SCORES[i]
     return score
 
 
-
-def max_word_value(maxword=DICTIONARY):
+def max_word_value(maxword=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    score = 0
+    word = ""
+    for i in maxword:
+        if calc_word_value(i) > score:
+            score = calc_word_value(i)
+            word = i
+    return word
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    pass
