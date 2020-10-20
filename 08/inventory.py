95ca36ec5492c450b2473744356c09fbb877515b
Austin Laturno <alaturno@gmail.com>
2020-10-19 17:22:13 -0700

    all test passed!

---
+++ 08/inventory.py
@@ -0,0 +1,30 @@
+# Having home and contents insurance requires you to accurately indicate the value of the items in your house. The idea
+# came to me when doing this for myself.
+# What if I could write a simple program that allowed me to create a list of rooms in my house and for each room a list
+# of items along with their dollar values?
+# House Inventory Tracker Requirements
+#     Create a list of rooms (doesn't have to use the list type).
+#     Each room in your rooms list needs to contain at least 5 items (ie, TV, couch, table, etc) and the relative dollar
+#     value of each item.
+#     The script you will write will print out each room along with the individual items and values. This needs to be
+#     properly formatted, eg: no printing a dict as is.
+#
+# Bonus
+# These are bonus features. Not required but cool to try if you're interested:
+#     Create some sort of program shell with a menu system around this. ie, "Which room would you like to list out?"
+#     If you're really game, allow users to create rooms and update information.
+#     You could even make an API with Flask or your preferred framework
+#     Print the total dollar value of each room and the entire house.
+#     Have persistent storage of the data. sqlite3 = stdlib and light-weight, but feel free to use your preferred
+#     DB / module.
+
+
+
+def insuredItems():
+    rooms = ['bedroom', 'living room', 'kitchen']
+
+def addItems(item, price):
+    item == input('Please list item name')
+    price == input('Please list price')
+
+
